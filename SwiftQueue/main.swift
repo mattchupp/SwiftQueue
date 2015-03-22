1f3c834787ded84fbf9465dce908fcd988c8627c
Matthew Chupp <mattlchupp@gmail.com>
2015-03-22 01:09:28 -0500

    Initial Commit

---
+++ SwiftQueue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftQueue
+//
+//  Created by Matthew Chupp on 3/22/15.
+//  Copyright (c) 2015 MattChupp. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
